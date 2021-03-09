20ccaf8c688eb6d6852173ddb3c672bbf97c190b
Lucas Backes <lucas.backes07@gmail.com>
2021-03-09 12:14:35 +0100

    fait jusqu'à 8 mais à check

--- implementation.py
+++ implementation.py
@@ -1,11 +1,11 @@
 import math
-
+import numpy as np
 def entropy(prob):
 	
 	entr = 0
 	for i in range(len(prob)):
 		#entr += prob[i]*math.log(1/prob[i], 2)
-		entr += prob[i]*math.log(prob[i])
+		entr += prob[i]*math.log(prob[i], 2)
 	
 	return -entr
 	
@@ -14,7 +14,7 @@ def joint_entropy(joint_prob):
 	
 	joint_entr = 0
 	for i in range(len(joint_prob)):
-		joint_entr += joint_prob[i]*math.log(joint_prob[i])
+		joint_entr += joint_prob[i]*math.log(joint_prob[i], 2)
 	
 	return -joint_prob
 
@@ -22,23 +22,29 @@ def joint_entropy(joint_prob):
 def conditional_entropy(cond_prob, marg_prob):
 	
 	cond_entr = 0
-	for i in range(len(cond_prob)):
-		cond_entr += cond_prob[i]*marg_prob[i]*math.log(cond_prob[i])
+	for i in range(cond_prob.shape[0]):
+		for j in range(cond_prob.shape[1]):
+			cond_entr += cond_prob[i][j]*marg_prob[i]*math.log(cond_prob[i][j], 2)
 	
 	return -cond_entr
 
 
 def mutual_information(prob_X, prob_Y, joint_prob):
 	
+	prob_X = [0.603, 0.3005, 0.0965]
+	prob_Y = [0.508, 0.492]
+	joint_prob = np.array([[0.3115, 0.049 , 0.1475],
+						   [0.2915, 0.0475, 0.153 ]])
+	
 	mut_inf = 0
-	for i in range(len(prob_X)):
-		for j in range(len(prob_Y)):
-			mut_inf += joint_prob[i+j]*math.log(joint_prob[i+j]/(prob_X[i]*prob_Y[j]))
+	for i in range(len(prob_Y)):
+		for j in range(len(prob_X)):
+			mut_inf += joint_prob[i][j]*math.log(joint_prob[i][j]/(prob_X[j]*prob_Y[i]), 2)
 
 	return mut_inf
 
 prob = [3/8, 2/8, 3/8]
 e = entropy(prob)
-print(e)
+#print(e)
 
 
--- medical.py
+++ medical.py
@@ -9,7 +9,6 @@ def get_prob_distribution(variable):
 	prob = []
 	N = df.shape[0]
 	card = len(df[variable].value_counts())
-	print(df[variable].value_counts())
 	
 	for i in range(card):
 		p = df[variable].value_counts()[i]/N
@@ -18,22 +17,100 @@ def get_prob_distribution(variable):
 	return prob
 	
 
+def get_cardinality(variable):
+	return len(df[variable].value_counts())
+	
 # à tester avec 2 autres variables
-def get_cond_prob(variable, given):
+def get_cond_prob(variable, given, disp = False):
 	
 	card_variable = len(df[variable].value_counts())
 	card_given = len(df[given].value_counts())
 	prob = np.zeros((card_given , card_variable))
 	
-	
 	for i in range(card_given):
+		N = df[given].value_counts()[i]
 		for j in range(card_variable):
-			prob[i][j] = 	df[df[given] == df[given].value_counts().index.tolist()[i]][variable].value_counts()[j]
+			name_var = df[given].value_counts().index.tolist()[i]
+			if disp:
+				print(name_var)
+			prob[i][j] = df[df[given] == name_var][variable].value_counts(sort = False)[j]/N
 
+	df[variable].value_counts(sort = False)
+
+	return prob
+
+def get_joint_prob(variable_1, variable_2):
 	
-if __name__ == "__main__":
-	prob = get_prob_distribution('obesity')
-	print(prob)
-	entr = f.entropy(prob)
-	print(entr)
+	card_variable_1 = len(df[variable_1].value_counts())
+	card_variable_2 = len(df[variable_2].value_counts())
+	joint_prob = np.zeros((card_variable_2 , card_variable_1))
+	
+	cond_prob = get_cond_prob(variable_1, variable_2)
+	marg_prob = get_prob_distribution(variable_2)
+	
+	for i in range(cond_prob.shape[0]):
+		for j in range(cond_prob.shape[1]):
+			joint_prob[i][j] =  cond_prob[i][j]*marg_prob[i]
+	
+	return joint_prob
+	
+if __name__ == "__main__":	
+	
+	
+	variable = 'DIS'
+	given = '
+	p = get_cond_prob('DIS', 'sex', True)
+	
+	
+	# Entropy of each variable
+	card = []
+	entropies = []
+	
+	for variable in list(df.columns):
+		card.append(get_cardinality(variable))
+		prob_dist = get_prob_distribution(variable)
+		entropy = f.entropy(prob_dist)
+		entropies.append(round(entropy, 3))
+		
+	print(card)	
+	print(entropies)
+	
+	# Conditional entropy of the disease given each variables	
+	
+	cond_entropies = []
+	variable = 'DIS'
+	
+	for given in list(df.columns):
+		if given == variable:
+			continue
+		cond_prob = get_cond_prob(variable, given)
+		marg_prob = get_prob_distribution(given)
+		cond_entr = f.conditional_entropy(cond_prob, marg_prob)
+		cond_entropies.append(round(cond_entr, 3))
+		
+	
+	print(cond_entropies)
+	
+	# Mutual information
+	
+	variable_1 = 'obesity'
+	variable_2 = 'age'
+	
+	joint_prob = get_joint_prob(variable_1, variable_2)
+	prob_var_1 = get_prob_distribution(variable_1)
+	prob_var_2 = get_prob_distribution(variable_2)
+	mut_inf = f.mutual_information(prob_var_1, prob_var_2, joint_prob)
+	
+	cond_prob = get_cond_prob('obesity', 'age')
+	marg_prob = get_prob_distribution('age')
+	cond_entr = f.conditional_entropy(cond_prob, marg_prob)
+	
+	
+	
+	p = get_prob_distribution('obesity')
+	entr = f.entropy(p)
+	
+	
+	
+	
 	
